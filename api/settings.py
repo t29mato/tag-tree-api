41091385cf80a545addf3fca863954ace7baadf9
t29mato <tomoya.matou@gmail.com>
2021-06-21 08:20:44 +0000

    ci: set up VSCode remote containre & Djongo

--- api/settings.py
+++ api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'test',
+        'ENFORCE_SCHEME': False,
+        'CLIENT': {
+            'host': 'mongodb://mongodb',
+            'port': 27018
+        }
     }
 }
 
