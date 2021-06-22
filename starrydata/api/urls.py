2353f43af07fc7391b7855193519cf0a618af040
t29mato <tomoya.matou@gmail.com>
2021-06-22 08:32:16 +0000

    refacor: fabrication processes API name

--- starrydata/api/urls.py
+++ starrydata/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from starrydata.api import views
 
 urlpatterns = [
-    path('processes/', views.ListView.as_view(), name='processList'),
-    path('processes/<int:pk>', views.DetailView.as_view(), name='processDetail')
+    path('fabrication_processes/', views.ListView.as_view(), name='processList'),
+    path('fabrication_processes/<int:pk>', views.DetailView.as_view(), name='processDetail')
 ]
