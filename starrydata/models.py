f9acaf175b93893bc5ae26b9cc8a338663c7eadf
t29mato <tomoya.matou@gmail.com>
2021-06-22 08:32:52 +0000

    feat(FabricationProcessAPI): set foreign key

--- starrydata/models.py
+++ starrydata/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class FabricationProcess(models.Model):
     name_ja = models.CharField(max_length=100)
-    parent_id = models.CharField(max_length=12, null=True)
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name_ja
