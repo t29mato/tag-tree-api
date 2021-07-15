cc10fe4392e3b249a87c698e5d8acbc92e8276e4
t29mato <tomoya.matou@gmail.com>
2021-07-15 01:17:01 +0000

    feat(import initial data): raise exception if the text is empty.

--- starrydata/management/commands/import_initial_data_polymer.py
+++ starrydata/management/commands/import_initial_data_polymer.py
@@ -25,11 +25,14 @@ class Command(BaseCommand):
             print('ルートノードが存在しないのでルートノードを作成')
         list(map(lambda parent: self.importTree(root_node['node_id'], parent), parents))
 
-    def importTree(self, parent_id: int,tree: Tree):
+    def importTree(self, parent_id: int, tree: Tree):
         print('処理開始：「' + tree['text'] + '」')
         parent = PolymerNode.objects.get(pk=parent_id)
 
         tag = PolymerTag.objects.filter(name=tree['text'])
+
+        if tree['text'] == '':
+            raise ValueError("空テキストが存在するため処理を中止 - ノードID：", tree['id'])
 
         if tag.exists():
             node = PolymerNode.objects.filter(polymer_tag=tag[0], parent=parent)
@@ -38,15 +41,15 @@ class Command(BaseCommand):
             else:
                 newNode = PolymerNode(polymer_tag=tag[0],parent=parent)
                 newNode.save()
-                print('新規ノード生成完了：「' + parent.polymer_tag.name + '」')
+                print('新規ノード生成 - 親：「' + parent.polymer_tag.name + '」')
         else:
             newTag = PolymerTag(name=tree['text'])
             newTag.save()
-            print('新規タグ生成完了：「' + newTag.name + '」')
+            print('新規タグ生成：「' + newTag.name + '」')
             newNode = PolymerNode(polymer_tag=tag[0],parent=parent)
             newNode.save()
-            print('新規ノード生成完了 - 親：「' + parent.polymer_tag.name + '」')
-        print('処理終了：「' + tree['text'] + '」\n')
+            print('新規ノード生成 - 親：「' + parent.polymer_tag.name + '」')
+        print('')
 
         node = PolymerNode.objects.get(polymer_tag=tag[0], parent=parent)
         if 'children' in tree:
