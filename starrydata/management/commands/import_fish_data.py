404c519a47fce0a219b290ad01e04e389a5812e5
t29mato <tomoya.matou@gmail.com>
2021-08-02 08:57:00 +0000

    refactor(ImportFishData): update standard output

--- starrydata/management/commands/import_fish_data.py
+++ starrydata/management/commands/import_fish_data.py
@@ -6,22 +6,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         csv_file = open("starrydata/management/commands/fish-tree.csv", "r", encoding="UTF-8", errors="", newline="")
         f = csv.DictReader(csv_file, delimiter=",", doublequote=False, lineterminator="\r\n", quotechar="'", skipinitialspace=True)
-        for row in f:
+        for index, row in enumerate(f):
+            print(str(index) + '：' + row["category_name"])
             words = Word.objects.filter(name=row["category_name"])
             if not words.exists():
                 word = Word(name=row["category_name"], language=Word.Language.JAPANESE)
                 word.save()
-                print('単語追加：「' + row["category_name"] + '」')
+                print('単語追加')
             else:
-                print('単語追加なし：「' + row["category_name"] + '」')
+                print('単語追加なし')
 
             tags = Tag.objects.filter(word_ja__name=row["category_name"])
             if not tags.exists():
                 tag = Tag(word_ja=words[0])
                 tag.save()
-                print('タグ追加：「' + row["category_name"] + '」')
+                print('タグ追加')
             else:
-                print('タグ追加なし：「' + row["category_name"] + '」')
+                print('タグ追加なし')
 
 
             nodes = Node.objects.filter(tag__word_ja=words[0])
@@ -29,12 +30,12 @@ class Command(BaseCommand):
                 if row["parent_id"] == '0':
                     rootNode = Node(tag=tags[0], id=row["id"], parent=None)
                     rootNode.save()
-                    print('ルートノード追加：「' + row["category_name"] + '」')
+                    print('ルートノード追加')
                     continue
                 parent = Node.objects.get(id=row["parent_id"])
                 node = Node(tag=tags[0], id=row["id"], parent=parent)
                 node.save()
-                print('ノード追加：「' + row["category_name"] + '」')
+                print('ノード追加')
             else:
-                print('ノード追加なし：「' + row["category_name"] + '」')
+                print('ノード追加なし')
             print()
