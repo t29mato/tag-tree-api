41a21c956b2493c71a01be55f3a8d1a476dc7d65
t29mato <tomoya.matou@gmail.com>
2022-01-25 07:12:49 +0000

    fix: it was redundant

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
