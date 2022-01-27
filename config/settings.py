c4406bc2b5f411f165584007bb60b01c1912aa7e
t29mato <tomoya.matou@gmail.com>
2022-01-27 10:42:06 +0000

    ci(cors): add vercel domain

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'https://std3-user-front.vercel.app'
+    'https://tag-tree.vercel.app',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
