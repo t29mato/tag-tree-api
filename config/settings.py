dda0f95dce65cf3cf2db6ac8e897115941acf461
t29mato <tomoya.matou@gmail.com>
2022-01-27 11:28:59 +0000

    ci(cors): add development environment by vercel

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['tag-tree.herokuapp.com']
+ALLOWED_HOSTS = ['tag-tree.herokuapp.com', 'tag-tree-api-development.herokuapp.com']
 
 # Application definition
 
